f93cae7d593dfe566972539233b21b4c148e67a0
RS <zziivv77@gmail.com>
2026-02-12 16:53:09 +0500

    Work6

--- dag/plugins/churn.py
+++ dag/plugins/churn.py
@@ -60,12 +60,38 @@ def extract(**kwargs):
 
 def transform(**kwargs):
     ti = kwargs['ti']
+
     data = ti.xcom_pull(task_ids='extract', key='extracted_data')
 
     feature_cols = data.columns.drop('customer_id').tolist()
     is_duplicated_features = data.duplicated(subset=feature_cols, keep=False)
-    
     data = data[~is_duplicated_features].reset_index(drop=True)
+
+    cols_with_nans = data.isnull().sum()
+    cols_with_nans = cols_with_nans[cols_with_nans > 0].index.drop('end_date')
+    for col in cols_with_nans:
+        if data[col].dtype in [float, int]:
+            fill_value = data[col].median() if not data[col].isna().all() else 0
+        elif data[col].dtype == 'object':
+            fill_value = data[col].mode().iloc[0] if not data[col].mode().empty else 'Unknown'
+        data[col] = data[col].fillna(fill_value)
+        
+    # num_cols = data.select_dtypes(['float']).columns
+    # threshold = 1.5
+    # potential_outliers = pd.DataFrame()
+
+    # for col in num_cols:
+    #     Q1 = data[col].quantile(0.25)
+    #     Q3 = data[col].quantile(0.75)
+    #     IQR = Q3 - Q1
+    #     margin = threshold * IQR
+    #     lower = Q1 - margin
+    #     upper = Q3 + margin
+    #     potential_outliers[col] = ~data[col].between(lower, upper)
+
+    # outliers = potential_outliers.any(axis=1)
+    
+    # data = data[~outliers].reset_index(drop=True)
 
     ti.xcom_push('transformed_data', data)
 
